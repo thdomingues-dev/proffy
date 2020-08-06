8a37e780b3f1520c3d62171cf3e496b7339e78af
Thales Domingues <thdomingues.dev@gmail.com>
2020-08-05 22:18:16 -0400

    proffy

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,31 +3,56 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: string;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/55956799?s=460&u=48f1deaf5346122eb1fbe89e18f45324363becef&v=4" alt="Thales Domingues" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Thales Domingues</strong>
-          <span>Algoritmos</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Descriação.
-          </p>
+        {teacher.bio}
+      </p>
 
       <footer>
         <p>
           Preço/hora
-              <strong>R$ 80,00</strong>
+              <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
                 Entrar em contato
-              </button>
+        </a>
       </footer>
     </article>
   );
